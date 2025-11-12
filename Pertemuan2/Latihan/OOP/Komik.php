75eef3e0510d52e283fe250d8037e149d1485f1d
dwiyyl <dwiyulianti327@gmail.com>
2025-11-12 12:09:21 +0700

    mengerjakan tugas

---
+++ Pertemuan2/Latihan/OOP/Komik.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once _DIR_ . '/Produk.php';
+
+class Komik extends Produk {
+    public $jmlHalaman;
+
+    public function __construct($judul, $penulis, $penerbit, $harga, $jmlHalaman) {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduk() {
+        $str = "Komik : " . parent::getLabel() . " | (Rp. " . $this->harga . ") - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+?>
